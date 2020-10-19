13f450a33219ca51fa009e59c20f7633ba575db8
Juan Emilio Zurita Macias <juan.macias@sa.catapult.org.uk>
2020-10-19 11:06:20 +0100

    Adapt restore script to new data volumes

--- restore_era5_land_data.sh
+++ restore_era5_land_data.sh
@@ -1,21 +1,3 @@
 #!/bin/bash
 
-# daily data part 1
-aws --endpoint-url=$S3_URL s3 sync s3://csvs-netcdf/ERA5-Land/daily/ /usr/local/era5-land/daily-1-data/ \
---exclude "*"
---include ERA5-Land_daily_mean_2mTemp/* \
---include ERA5-Land_daily_mean_RH/* && \
-
-# daily data part 2
-aws --endpoint-url=$S3_URL s3 sync s3://csvs-netcdf/ERA5-Land/daily/ /usr/local/era5-land/daily-2-data/ \
---exclude "*"
---include ERA5-Land_daily_mean_TotalWind/* \
---include ERA5-Land_daily_total_precipitation/* && \
-
-# monthly data
-mkdir /usr/local/era5-land/monthly-yearly-data/monthly &&
-aws --endpoint-url=$S3_URL s3 sync s3://csvs-netcdf/ERA5-Land/monthly/ /usr/local/era5-land/monthly-yearly-data/monthly &&
-
-# 30 yeras average data
-mkdir /usr/local/era5-land/monthly-yearly-data/30year &&
-aws --endpoint-url=$S3_URL s3 sync s3://csvs-netcdf/ERA5-Land/30year/ /usr/local/era5-land/monthly-yearly-data/30year
+aws --endpoint-url=$S3_URL s3 sync s3://csvs-netcdf/ERA5-Land/ /usr/local/era5-land/
