bb4244337d22a5d8419cce0e224d367fa46f023e
James <james.hinton@live.co.uk>
2023-03-09 15:43:41 +0000

    cicd: removed mkdir from scripts

--- scripts/restore_gpcp_data.sh
+++ scripts/restore_gpcp_data.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-
-mkdir -p /usr/local/gpcp
 azcopy copy "https://csvsdata.blob.core.windows.net/csvs-netcdf/gpcp/?$SAS_TOKEN" /usr/local/gpcp/ --recursive=true --from-to BlobLocal --as-subdir=False
--- scripts/restore_ncep_data.sh
+++ scripts/restore_ncep_data.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-
-mkdir -p /usr/local/ncep
 azcopy copy "https://csvsdata.blob.core.windows.net/csvs-netcdf/ncep/?$SAS_TOKEN" /usr/local/ncep/ --recursive=true --from-to BlobLocal --as-subdir=False
