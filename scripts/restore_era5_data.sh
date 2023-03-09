9d2260b06cc7bdcd2620b78d138a9bae67b7da51
James <james.hinton@live.co.uk>
2023-03-09 17:20:30 +0000

    cicd: updated era5 data shell

--- scripts/restore_era5_data.sh
+++ scripts/restore_era5_data.sh
@@ -1,29 +1,32 @@
 #!/bin/bash
 
 # daily data part 1
-aws --endpoint-url=$S3_URL s3 sync s3://csvs-netcdf/daily/ /usr/local/daily-1-data/ \
---exclude ERA5_daily_mean_m* \
---exclude ERA5_daily_mean_s* \
+azcopy copy "https://csvsdata.blob.core.windows.net/csvs-netcdf/daily/?$SAS_TOKEN" /usr/local/daily-1-data/
+--recursive=true
+--exclude ERA5_daily_mean_m*
+--exclude ERA5_daily_mean_s*
 --exclude ERA5_daily_mean_SST* &&
 
 # daily data part 2
-aws --endpoint-url=$S3_URL s3 sync s3://csvs-netcdf/daily/ /usr/local/daily-2-data/ \
---exclude "*" --include ERA5_daily_mean_ms* --include ERA5_daily_mean_soil_temp_* \
+azcopy copy "https://csvsdata.blob.core.windows.net/csvs-netcdf/daily/?$SAS_TOKEN" /usr/local/daily-2-data/
+--recursive=true
+--exclude "" --include ERA5_daily_mean_ms --include ERA5_daily_mean_soil_temp_*
 --include ERA5_daily_mean_SST* &&
 
 # daily data part 3
-aws --endpoint-url=$S3_URL s3 sync s3://csvs-netcdf/daily/ /usr/local/daily-3-data/ \
---exclude ERA5_daily_mean_2mTemp/* \
---exclude ERA5_daily_mean_RH/* \
---exclude ERA5_daily_mean_TotalWind/* \
---exclude ERA5_daily_mean_mslp/* \
---exclude ERA5_daily_mean_soil_temp_L1/* \
+azcopy copy "https://csvsdata.blob.core.windows.net/csvs-netcdf/daily/?$SAS_TOKEN" /usr/local/daily-3-data/
+--recursive=true
+--exclude ERA5_daily_mean_2mTemp/*
+--exclude ERA5_daily_mean_RH/*
+--exclude ERA5_daily_mean_TotalWind/*
+--exclude ERA5_daily_mean_mslp/*
+--exclude ERA5_daily_mean_soil_temp_L1/*
 --exclude ERA5_daily_mean_SST/* &&
 
 # monthly data
 mkdir -p /usr/local/monthly-yearly-data/monthly &&
-aws --endpoint-url=$S3_URL s3 sync s3://csvs-netcdf/monthly/ /usr/local/monthly-yearly-data/monthly &&
+azcopy copy "https://csvsdata.blob.core.windows.net/csvs-netcdf/monthly/?$SAS_TOKEN" /usr/local/monthly-yearly-data/monthly --recursive=true &&
 
 # 30 yeras average data
 mkdir -p /usr/local/monthly-yearly-data/30year &&
-aws --endpoint-url=$S3_URL s3 sync s3://csvs-netcdf/30year/ /usr/local/monthly-yearly-data/30year
+azcopy copy "https://csvsdata.blob.core.windows.net/csvs-netcdf/30year/?$SAS_TOKEN" /usr/local/monthly-yearly-data/30year --recursive=true
