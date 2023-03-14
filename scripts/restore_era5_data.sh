c9f0e857d17d620ac50641b0e0b68120968bfadd
James <james.hinton@live.co.uk>
2023-03-14 11:40:38 +0000

    cicd: updated era5 loader

--- scripts/restore_era5_data.sh
+++ scripts/restore_era5_data.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 # daily data part 1
-azcopy copy "https://csvsdata.blob.core.windows.net/csvs-netcdf/daily/?$SAS_TOKEN" /usr/local/daily-1-data/ --recursive=true --exclude-pattern "ERA5_daily_mean_m*" --exclude-pattern "ERA5_daily_mean_s*" --exclude-pattern "ERA5_daily_mean_SST*" --from-to BlobLocal --as-subdir=False &&
+azcopy copy "https://csvsdata.blob.core.windows.net/csvs-netcdf/daily/?$SAS_TOKEN" /usr/local/daily-1-data/ --overwrite=false --recursive=true --exclude-pattern "ERA5_daily_mean_m*" --exclude-pattern "ERA5_daily_mean_s*" --exclude-pattern "ERA5_daily_mean_SST*" --from-to BlobLocal --as-subdir=False &&
 
 # daily data part 2
-azcopy copy "https://csvsdata.blob.core.windows.net/csvs-netcdf/daily/?$SAS_TOKEN" /usr/local/daily-2-data/ --recursive=true --include-pattern "ERA5_daily_mean_ms" --include-pattern "ERA5_daily_mean_soil_temp_*" --include-pattern "ERA5_daily_mean_SST*" --from-to BlobLocal --as-subdir=False &&
+azcopy copy "https://csvsdata.blob.core.windows.net/csvs-netcdf/daily/?$SAS_TOKEN" /usr/local/daily-2-data/ --overwrite=false --recursive=true --include-pattern "ERA5_daily_mean_ms" --include-pattern "ERA5_daily_mean_soil_temp_*" --include-pattern "ERA5_daily_mean_SST*" --from-to BlobLocal --as-subdir=False &&
 
 # daily data part 3
-azcopy copy "https://csvsdata.blob.core.windows.net/csvs-netcdf/daily/?$SAS_TOKEN" /usr/local/daily-3-data/ --recursive=true --exclude-pattern "ERA5_daily_mean_2mTemp/*" --exclude-pattern "ERA5_daily_mean_RH/*" --exclude-pattern "ERA5_daily_mean_TotalWind/*" --exclude-pattern "ERA5_daily_mean_mslp/*" --exclude-pattern "ERA5_daily_mean_soil_temp_L1/*" --exclude-pattern "ERA5_daily_mean_SST/*" --from-to BlobLocal --as-subdir=False &&
+azcopy copy "https://csvsdata.blob.core.windows.net/csvs-netcdf/daily/?$SAS_TOKEN" /usr/local/daily-3-data/ --overwrite=false --recursive=true --exclude-pattern "ERA5_daily_mean_2mTemp/*" --exclude-pattern "ERA5_daily_mean_RH/*" --exclude-pattern "ERA5_daily_mean_TotalWind/*" --exclude-pattern "ERA5_daily_mean_mslp/*" --exclude-pattern "ERA5_daily_mean_soil_temp_L1/*" --exclude-pattern "ERA5_daily_mean_SST/*" --from-to BlobLocal --as-subdir=False &&
 
 # monthly data
 mkdir -p /usr/local/monthly-yearly-data/monthly &&
-azcopy copy "https://csvsdata.blob.core.windows.net/csvs-netcdf/monthly/?$SAS_TOKEN" /usr/local/monthly-yearly-data/monthly --recursive=true --from-to BlobLocal --as-subdir=False &&
+azcopy copy "https://csvsdata.blob.core.windows.net/csvs-netcdf/monthly/?$SAS_TOKEN" /usr/local/monthly-yearly-data/monthly --overwrite=false --recursive=true --from-to BlobLocal --as-subdir=False &&
 
 # 30 yeras average data
 mkdir -p /usr/local/monthly-yearly-data/30year &&
-azcopy copy "https://csvsdata.blob.core.windows.net/csvs-netcdf/30year/?$SAS_TOKEN" /usr/local/monthly-yearly-data/30year --recursive=true --from-to BlobLocal --as-subdir=False 
+azcopy copy "https://csvsdata.blob.core.windows.net/csvs-netcdf/30year/?$SAS_TOKEN" /usr/local/monthly-yearly-data/30year --overwrite=false --recursive=true --from-to BlobLocal --as-subdir=False 
